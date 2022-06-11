6c21d4c46e29cc555be7212263bd72c20a6caa2f
Shihao-Zhong <zhong950419@gmail.com>
2022-06-11 17:34:23 -0400

    add vaif support

--- hack/build-image.sh
+++ hack/build-image.sh
@@ -4,9 +4,16 @@ set -eux
 echo
 echo "Start build images, Repo: $1, Tag: $2"
 echo
+
+root_directory=$(echo "$PWD")
+echo $root_directory
+
 for dir in ts-*; do
     if [[ -d $dir ]]; then
         if [[ -n $(ls "$dir" | grep -i Dockerfile) ]]; then
+            mkdir -p $dir/target
+            cp $root_directory/opentelemetry/opentelemetry-javaagent-all.jar $dir/target/otel.jar
+            cp $root_directory/opentelemetry/vaif.json $dir/target/vaif.json
             echo "build ${dir}"
             docker build -t "$1"/"${dir}" "$dir"
             docker tag "$1"/"${dir}":latest "$1"/"${dir}":"$2"
