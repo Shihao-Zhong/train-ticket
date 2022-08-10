97103942cc7f5c37964dbfa6435a907a60579d01
Shihao-Zhong <zhong950419@gmail.com>
2022-08-09 21:46:54 -0400

    update workflow

--- script/publish-docker-images.sh
+++ script/publish-docker-images.sh
@@ -8,6 +8,10 @@ for dir in ts-*; do
     if [[ -d $dir ]]; then
         if [[ -n $(ls "$dir" | grep -i Dockerfile) ]]; then
             echo "build ${dir}"
+            cp $root_directory/opentelemetry/opentelemetry-javaagent-all.jar $dir/target/otel.jar
+            cp $root_directory/opentelemetry/json.jar $dir/target/json.jar
+            cp $root_directory/opentelemetry/vaif.json $dir/target/vaif.json
+            cp $root_directory/opentelemetry/enable.json $dir/target/enable.json
 	    # Must use `buildx` as docker build tool
             docker build --push -t "$1"/"${dir}":"$2" "$dir"
         fi
