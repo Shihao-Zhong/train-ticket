4d6f00e4acb7f36618700433eb90839b0299fbb8
Shihao-Zhong <zhong950419@gmail.com>
2022-08-09 22:17:08 -0400

    update config

--- script/publish-docker-images.sh
+++ script/publish-docker-images.sh
@@ -12,7 +12,7 @@ for dir in ts-*; do
     if [[ -d $dir ]]; then
         if [[ -n $(ls "$dir" | grep -i Dockerfile) ]]; then
             echo "build ${dir}"
-            cp ../opentelemetry/opentelemetry-javaagent-all.jar $dir/target/otel.jar
+            cp ../opentelemetry/opentelemetry-javaagent-all-work.jar $dir/target/otel.jar
             cp ../opentelemetry/json.jar $dir/target/json.jar
             cp ../opentelemetry/vaif.json $dir/target/vaif.json
             cp ../opentelemetry/enable.json $dir/target/enable.json
