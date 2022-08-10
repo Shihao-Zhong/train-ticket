4bd1a539040bc1450b2918f6becb49f804fdea1f
Shihao-Zhong <zhong950419@gmail.com>
2022-08-09 21:55:49 -0400

    update workflow

--- script/publish-docker-images.sh
+++ script/publish-docker-images.sh
@@ -3,6 +3,7 @@ set -eux
 
 root_directory=$(echo "$PWD")
 echo $root_directory
+ls $root_directory
 
 echo
 echo "Publishing images, Repo: $1, Tag: $2"
@@ -11,10 +12,10 @@ for dir in ts-*; do
     if [[ -d $dir ]]; then
         if [[ -n $(ls "$dir" | grep -i Dockerfile) ]]; then
             echo "build ${dir}"
-            cp $root_directory/opentelemetry/opentelemetry-javaagent-all.jar $dir/target/otel.jar
-            cp $root_directory/opentelemetry/json.jar $dir/target/json.jar
-            cp $root_directory/opentelemetry/vaif.json $dir/target/vaif.json
-            cp $root_directory/opentelemetry/enable.json $dir/target/enable.json
+            cp ../opentelemetry/opentelemetry-javaagent-all.jar $dir/target/otel.jar
+            cp ../opentelemetry/json.jar $dir/target/json.jar
+            cp ../opentelemetry/vaif.json $dir/target/vaif.json
+            cp ../opentelemetry/enable.json $dir/target/enable.json
 	    # Must use `buildx` as docker build tool
             docker build --push -t "$1"/"${dir}":"$2" "$dir"
         fi
