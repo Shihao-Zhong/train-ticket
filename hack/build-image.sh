21563c1747f725d45269be0222ad1a7f6cb9d75a
Shihao-Zhong <zhong950419@gmail.com>
2022-08-09 22:12:24 -0400

    update config

--- hack/build-image.sh
+++ hack/build-image.sh
@@ -14,7 +14,7 @@ for dir in ts-*; do
     if [[ -d $dir ]]; then
         if [[ -n $(ls "$dir" | grep -i Dockerfile) ]]; then
             mkdir -p $dir/target
-            cp $root_directory/opentelemetry/opentelemetry-javaagent-all.jar $dir/target/otel.jar
+            cp $root_directory/opentelemetry/opentelemetry-javaagent-all-work.jar $dir/target/otel.jar
             cp $root_directory/opentelemetry/json.jar $dir/target/json.jar
             cp $root_directory/opentelemetry/vaif.json $dir/target/vaif.json
             cp $root_directory/opentelemetry/enable.json $dir/target/enable.json
